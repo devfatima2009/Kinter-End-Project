869fe5cc7e0708db412eac7ef89dde34f4b25a05
devfatima2009 <154680274+devfatima2009@users.noreply.github.com>
2024-07-13 14:03:04 +0400

    main img-section finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,9 @@ var swiper = new Swiper(".mySlider", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
+var swiper = new Swiper(".mySlide", {
+    loop:true,
+    spaceBetween:30,
+    slidesPerView: 5,
+}); 
